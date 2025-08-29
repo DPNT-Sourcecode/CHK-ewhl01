962152f01c84deec70737d6a79a8fbf8b768505b
sbx_user1051 <sbx_user1051@169.254.63.11>
2025-08-29 16:37:27 +0000

    Fri Aug 29 16:37:27 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -8,3 +8,12 @@ class TestSum():
     def test_sum_with_zero(self):
         assert SumSolution().compute(0, 10) == 10
 
+    def test_sum_upper_bound(self):
+        assert SumSolution().compute(100, 100) == 200
+
+    def test_sum_lower_bound(self):
+        assert SumSolution().compute(0, 0) == 0
+
+        
+
+
