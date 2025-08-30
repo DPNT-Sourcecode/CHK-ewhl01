8643650b6981a4f80cb7c67f43cee62c5abd047c
sbx_user1051 <sbx_user1051@169.254.52.113>
2025-08-30 09:29:50 +0000

    Sat Aug 30 09:29:50 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,6 +19,27 @@ class CheckoutSolution:
             "D": 15,
             "E": 40,
             "F": 10,
+            "G": 20,
+            "H": 10,
+            "I": 35,
+            "J": 60,
+            "K": 80,
+            "L": 90,
+            "M": 15,
+            "N": 40,
+            "O": 10,
+            "P": 50,
+            "Q": 30,
+            "R": 50,
+            "S": 20,
+            "T": 20,
+            "U": 40,
+            "V": 50,
+            "W": 20,
+            "X": 90,
+            "Y": 10,
+            "Z": 50,
+
         }
 
         # deals on some items
@@ -64,6 +85,7 @@ class CheckoutSolution:
                 total += count * prices[item]
 
         return total
+
 
 
 
